2574155e4a864d07fa5ef11b1f4ee74f2ff96004
Cédric Chambaz <chambazce@inf-pa512.etmlnet.local>
2016-04-22 14:20:14 +0200

    Première affichage (grossier) de la future application

---
+++ Test_Affichage/Test_AffichageTests/Test_AffichageTests.swift
@@ -0,0 +1,36 @@
+//
+//  Test_AffichageTests.swift
+//  Test_AffichageTests
+//
+//  Created by Cédric Chambaz on 18.04.16.
+//  Copyright © 2016 Cédric Chambaz. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Affichage
+
+class Test_AffichageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
